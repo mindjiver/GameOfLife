3cc7fb8856af4ee804ca5e33c644eaa6b41a5b48
Peter Joensson <peter.joensson@gmail.com>
2010-12-14 19:30:54 +0100

    Start of GLFW rendering.

--- gol.c
+++ gol.c
@@ -6,7 +6,7 @@
  * in the Software without restriction, including without limitation the right
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject o the following conditions:
+ * furnished to do so, subject to the following conditions:
  *  
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
@@ -27,10 +27,15 @@
 
 int main(int argc, char **argv)
 {
+	// TODO validate command line arguments:
+	//  - size of board
+	//  - simulation speed
+	//  ...
+	//  - Profit!
 	t_lifeBoard *board = createLifeBoard(10);
 	glfwInit();
 
-	// cleanup before we leave.
+	// Cleanup before we leave.
 	glfwTerminate();
 	destroyLifeBoard(board);
 }
