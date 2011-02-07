9a9bcabc624cb0d391102ad3746752a401448325
Peter Jönsson <peter.joensson@gmail.com>
2011-02-07 21:57:44 +0100

    Fixed bug with circular buffer being linear until fun effects occur.

--- list.c
+++ list.c
@@ -69,7 +69,7 @@ void addToCircularList(CircularList *list, void *data)
 	printf("Adding 0x%d\tto list 0x%d.\n", (int)data, (int)list);
 	fflush(NULL);
 //#endif
-//	list->nodes[list->next].value = data;
-// WTF? the pointer + 1 ? and then modulos the size of the list?
-//	list->next = list->next + 1 % list->size;
+	list->nodes[list->next].value = data;
+	// remove parenthesis for fun effects ;)
+	list->next = (list->next + 1) % list->size;
 }
